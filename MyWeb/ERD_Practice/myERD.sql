883779aa8cf9d21e54a1e8d6ee3d43528e6f5375
jihoo2002 <yolohoo@naver.com>
2023-10-27 19:45:14 +0900

    1027

---
+++ MyWeb/ERD_Practice/myERD.sql
@@ -0,0 +1,158 @@
+
+/* Drop Triggers */
+
+DROP TRIGGER TRI_board_bno;
+DROP TRIGGER TRI_reply_rno;
+
+
+
+/* Drop Tables */
+
+DROP TABLE admins CASCADE CONSTRAINTS;
+DROP TABLE reply CASCADE CONSTRAINTS;
+DROP TABLE board CASCADE CONSTRAINTS;
+DROP TABLE lec_order CASCADE CONSTRAINTS;
+DROP TABLE lecture CASCADE CONSTRAINTS;
+DROP TABLE members CASCADE CONSTRAINTS;
+
+
+
+/* Drop Sequences */
+
+DROP SEQUENCE SEQ_board_bno;
+DROP SEQUENCE SEQ_reply_rno;
+
+
+
+
+/* Create Sequences */
+
+CREATE SEQUENCE SEQ_board_bno INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE SEQ_reply_rno INCREMENT BY 1 START WITH 1;
+
+
+
+/* Create Tables */
+
+CREATE TABLE admins
+(
+	id varchar2(30) NOT NULL,
+	update_date date DEFAULT SYSDATE NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE board
+(
+	bno number(10,0) NOT NULL,
+	content varchar2(2000) NOT NULL,
+	writer varchar2(30) NOT NULL,
+	title varchar2(100),
+	PRIMARY KEY (bno)
+);
+
+
+CREATE TABLE lecture
+(
+	lec_no number(10,0) NOT NULL,
+	lec_subject varchar2(30) NOT NULL,
+	teacher varchar2(30),
+	PRIMARY KEY (lec_no)
+);
+
+
+CREATE TABLE lec_order
+(
+	id varchar2(30) NOT NULL,
+	lec_no number(10,0) NOT NULL,
+	ordar_date date DEFAULT SYSDATE,
+	UNIQUE (id, lec_no)
+);
+
+
+CREATE TABLE members
+(
+	id varchar2(30) NOT NULL,
+	name varchar2(30) NOT NULL,
+	age number(10,0) NOT NULL,
+	regdate date DEFAULT SYSDATE,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE reply
+(
+	rno number(10,0) NOT NULL,
+	content varchar2(2000),
+	writer varchar2(30) NOT NULL,
+	regdate date DEFAULT SYSDATE,
+	bno number(10,0) NOT NULL,
+	PRIMARY KEY (rno)
+);
+
+
+
+/* Create Foreign Keys */
+
+ALTER TABLE reply
+	ADD FOREIGN KEY (bno)
+	REFERENCES board (bno)
+;
+
+
+ALTER TABLE lec_order
+	ADD FOREIGN KEY (lec_no)
+	REFERENCES lecture (lec_no)
+;
+
+
+ALTER TABLE admins
+	ADD FOREIGN KEY (id)
+	REFERENCES members (id)
+;
+
+
+ALTER TABLE board
+	ADD FOREIGN KEY (writer)
+	REFERENCES members (id)
+;
+
+
+ALTER TABLE lec_order
+	ADD FOREIGN KEY (id)
+	REFERENCES members (id)
+;
+
+
+ALTER TABLE reply
+	ADD FOREIGN KEY (writer)
+	REFERENCES members (id)
+;
+
+
+
+/* Create Triggers */
+
+CREATE OR REPLACE TRIGGER TRI_board_bno BEFORE INSERT ON board
+FOR EACH ROW
+BEGIN
+	SELECT SEQ_board_bno.nextval
+	INTO :new.bno
+	FROM dual;
+END;
+
+/
+
+CREATE OR REPLACE TRIGGER TRI_reply_rno BEFORE INSERT ON reply
+FOR EACH ROW
+BEGIN
+	SELECT SEQ_reply_rno.nextval
+	INTO :new.rno
+	FROM dual;
+END;
+
+/
+
+
+
+
